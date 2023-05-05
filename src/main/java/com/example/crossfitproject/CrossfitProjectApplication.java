753bf20c2ebf8b17791b56682545f3a98e66bf25
Nickolodeon98 <sjeon0730@gmail.com>
2023-05-05 15:13:15 +0900

    feat: add a new project

---
+++ src/main/java/com/example/crossfitproject/CrossfitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.crossfitproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossfitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossfitProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crossfitproject/CrossfitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crossfitproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossfitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
